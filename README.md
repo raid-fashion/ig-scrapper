545ccd7ab24f7215be00456e3f386db4a009f8d1
Valentina Feruere <valentinafeve@gmail.com>
2023-02-08 16:51:31 -0500

    🕷️ Cleaning files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ig-hawker
+
+Builds an scrapper for instagram and generates a graph given a list of followers.
+
+![image](docs/graph.png)
--- scrapper.py
+++ scrapper.py
@@ -1,13 +1,12 @@
+import logging
 import pickle
+import time
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-import pandas as pd
-from selenium.webdriver.common.keys import Keys
-import time
 from selenium.common.exceptions import NoSuchElementException
-from pyvis.network import Network
-import logging
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
 MAIN_URL = "http://instagram.com"
 
@@ -20,7 +19,11 @@ class Scrapper:
     def __init__(self, username):
         self.username = username
         self.followers_tree = {}
-        self.main_driver = webdriver.Firefox()
+
+        options = FirefoxOptions()
+        # options.add_argument("--headless")
+
+        self.main_driver = webdriver.Firefox(options=options)
         return
 
     def start_and_log(self):
@@ -30,7 +33,7 @@ class Scrapper:
     def get_follower_usernames(self, username: str, n_scrolls=2):
         logging.info(f"Getting followers of {username}")
         self.main_driver.get(build_followers_url(username))
-        time.sleep(2)
+        time.sleep(5)
 
         # Looks for followers panel
         try:
@@ -67,14 +70,6 @@ class Scrapper:
         return followers
 
     def generate_followers_tree(self, username: str, depth=3, n_scrolls=2):
-        """
-        Generate followers tree given a user, that is to say, generates a tree of followers and its followers of depth 'depth'.
-        :param username:
-        :param depth: Depth of the tree
-        :param n_scrolls:
-        :return:
-        """
-
         if depth < 1:
             return
 
@@ -86,10 +81,11 @@ class Scrapper:
 
 
 def main():
-    scrapper = Scrapper("baru.derama")
+    user = "valentinafeve"
+    scrapper = Scrapper(user)
     scrapper.start_and_log()
     scrapper.generate_followers_tree(scrapper.username, depth=2, n_scrolls=40)
-    pickle.dump(scrapper.followers_tree, open("followers_tree_baruderama.pickle", "wb+"))
+    pickle.dump(scrapper.followers_tree, open(f"followers_tree_{user}.pickle", "wb+"))
     return
 
 
